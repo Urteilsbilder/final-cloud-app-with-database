c2ff00680928f96235d7b560fc6e0757ff807b96
Alexander Seiler <alexander_seiler@web.de>
2023-08-01 18:02:14 -0400

    task 2 submission view

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 # <HINT> Import any new Models here
+from .models import Course, Enrollment, Question, Choice, Submission
 from .models import Course, Enrollment
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
@@ -11,7 +12,6 @@ import logging
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 # Create your views here.
-
 
 def registration_request(request):
     context = {}
@@ -110,8 +110,19 @@ def enroll(request, course_id):
          # Collect the selected choices from exam form
          # Add each selected choice object to the submission object
          # Redirect to show_exam_result with the submission id
-#def submit(request, course_id):
+def submit(request, course_id):
+    course = get_object_or_404(Course, pk=course_id)
+    user = request.user
+    enrollment = Enrollment.objects.get(user=user, course=course)
+    submission = Submission.objects.create(enrollment_id=enrollment)
 
+    if request.method == "POST":
+        for key, value in request.POST.items():
+            if key.startswith('choice_'):
+                choice_id = int(key.split('_')[1])
+                choice = Choice.objects.get(id=choice_id)
+                submission.choices.add(choice)
+    return HttpResponseRedirect(reverse(viewname='onlinecourse:exam_result', args=(submission.id,)))
 
 # <HINT> A example method to collect the selected choices from the exam form from the request object
 #def extract_answers(request):
